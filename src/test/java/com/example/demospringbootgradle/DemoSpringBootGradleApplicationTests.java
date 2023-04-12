d38744ae90f315d03e32019e58fda85e17a56f7b
Leandro Souza <leandro.souza@avenuecode.com>
2023-04-12 18:07:06 -0300

    Initial commit

---
+++ src/test/java/com/example/demospringbootgradle/DemoSpringBootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringbootgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
